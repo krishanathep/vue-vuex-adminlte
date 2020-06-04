898fe36e028720b4fc19f2e6f31823010eec6f24
krishanathep <krishanathep@gmail.com>
2020-06-04 15:45:41 +0700

    Vue and AdminLTE

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-title">
+              Header
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="card-title">
+              Title
+            </div>
+            <div class="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat odio dignissimos excepturi libero veniam sit architecto vel accusantium assumenda, eos error voluptate alias, eaque iusto rem doloremque provident nesciunt recusandae?
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home" align='center'>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Vue.js + AdminLTE" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
